c5cd6f0b6fbc9c9f8d6b83d266d903f3d625329c
alex001584 <182679@upslp.edu.mx>
2023-07-13 08:17:52 -0600

    syntax optimisation

--- autoLumberjackV2.lua
+++ autoLumberjackV2.lua
@@ -9,65 +9,61 @@ function isSpruceWood()
 end
 
 function spiralPattern(isRight)
+
+    local turnDirection
     if isRight then
-        turtle.dig()
-        assert(turtle.forward(), "Bloqueo")
-        turtle.turnRight()
-        turtle.dig()
-        assert(turtle.forward(), "Bloqueo")
-
-        if turtle.detectUp() then
-            turtle.digUp()
-            assert(turtle.up(), "Bloqueo")
-            turtle.turnRight()
-            turtle.dig()
-            assert(turtle.forward(), "Bloqueo")
-            turtle.turnRight()
-            return true
-        end
-        return false
+        turnDirection = turtle.turnRight
     else
-        turtle.dig()
-        assert(turtle.forward(), "Bloqueo")
-        turtle.turnLeft()
-        turtle.dig()
-        assert(turtle.forward(), "Bloqueo")
-
-        if turtle.detectUp() then
-            turtle.digUp()
-            assert(turtle.up(), "Bloqueo")
-            turtle.turnLeft()
-            turtle.dig()
-            assert(turtle.forward(), "Bloqueo")
-            turtle.turnLeft()
-            return true
-        end
-        return false
+        turnDirection = turtle.turnLeft
     end
+
+    turtle.dig()
+    assert(turtle.forward(), "Bloqueo")
+    turnDirection()
+    turtle.dig()
+    assert(turtle.forward(), "Bloqueo")
+
+    if turtle.detectUp() then
+        turtle.digUp()
+        assert(turtle.up(), "Bloqueo")
+        turnDirection()
+        turtle.dig()
+        assert(turtle.forward(), "Bloqueo")
+        turnDirection()
+        return true
+    end
+    return false
+
 end
 
 if isSpruceWood() then
+
     turtle.dig()
     assert(turtle.forward(), "Bloqueo")
     turtle.dig()
     assert(turtle.forward(), "Bloqueo")
     turtle.turnRight()
 
-    local isBlockAbove
-    local height = 0
+    local isBlockAbove, isRight
+
     if isSpruceWood() then
-        repeat
-            isBlockAbove = spiralPattern(true)
-            height = height + 1
-        until not isBlockAbove
+        isRight = true
     else
         turtle.turnLeft()
         turtle.turnLeft()
-        repeat
-            isBlockAbove = spiralPattern(false)
-        until not isBlockAbove
+        isRight = false
     end
+
+    repeat
+        isBlockAbove = spiralPattern(isRight)
+    until not isBlockAbove
+
     print("Final del arbol")
+
+    while not turtle.detectDown() do
+        assert(turtle.down(), "Bloqueo")
+    end
+
 else
     print("Not Spruce")
 end
